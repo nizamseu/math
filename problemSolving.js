cf17860850be907e7d66698bfad64161891dac00
nizamseu <nizamcse.seu@gmail.com>
2021-08-09 00:23:49 +0600

    Total Sales Problem sovle with error handling

--- problemSolving.js
+++ problemSolving.js
@@ -24,17 +24,18 @@ function deliveryCost(item){
  // error cheking 
 
     if(typeof item == 'string' || typeof item == 'undefined' || typeof item == 'object'){
-        return 'NOT Allowed, Only Number allow'
+        return 'Give me a valid Number'
     }
 
     if( item <0){
         return 'Give me positive number'
     }
 
-
+    // calculating 
     const for_100=100;
     const for_200=80;
     const forMoreThan_200=50;
+
     if(item>200){
         const till_100 = 100*for_100;
         const moreThan_100 = 80*for_200;
@@ -56,4 +57,40 @@ function deliveryCost(item){
     }
 }
 
-console.log(deliveryCost(100));
+console.log(deliveryCost());
+
+
+function totalSales (shirts,pants,shoes){
+
+    // error handleing 
+        if((typeof shirts =='string' )|| (typeof pants =='string') || (typeof shoes =='string' )){
+            return 'Strins is not Allowed'
+        }
+        if((typeof shirts =='object' )|| (typeof pants =='object') || (typeof shoes =='object' )){
+            return 'Object is not Allowed'
+        }
+        if((typeof shirts =='undefined' )|| (typeof pants =='undefined') || (typeof shoes =='undefined' )){
+            return 'Check Your input number'
+        }
+
+// negative value checking 
+    if( shirts <0 || pants<0 ||shoes<0){
+        return 'Give me positive number'
+    }
+
+    const shirtPrice = 100;
+    const pantPrice= 200;
+    const shoePrice = 500;
+
+    // calculation 
+
+    const totalShirt = shirts*shirtPrice;
+    const totalPant = pants*pantPrice;
+    const totalShoe = shoes*shoePrice;
+
+    const totalAmount = totalShirt + totalPant + totalShoe;
+
+    return totalAmount;
+}
+
+console.log(totalSales(2,2,2));
